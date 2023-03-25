884f43ae06d78f62b2634893119b0247bc3193f2
FrancescoCheema <cekkosingh@gmail.com>
2023-03-24 17:06:32 -0700

    added function for round of game

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
 //Create Array to choose choices from
-const choices = ['Rock', 'Paper', 'Scissors']
+let choices = ['rock', 'paper', 'scissors']
 //Create function for computer to play
-function getComputerChoice(choices) {
+function getComputerChoice() {
     let choice = choices[Math.floor(Math.random() * choices.length)];
     return choice;
 }
+//Create function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "it's a tie";
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return ("You win! Rock beats scissors");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return ("You lose! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("You win! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return ("You lose! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return ("You win! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return ("You lose!Rock beats scissors");
+    }
+}
 
-console.log(getComputerChoice(choices));
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Choose your weapon: ")
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
